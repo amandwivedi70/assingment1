0113ebe46201644b6c4945f36d592f1dc6b6c382
amandwivedi70 <111753149+amandwivedi70@users.noreply.github.com>
2022-09-10 15:37:19 +0530

    Add files via upload

---
+++ practice_aasign_2.py
@@ -0,0 +1,58 @@
+# question 1 and 2
+# single line comment
+
+# this is a practice set
+
+# multi line comment
+'''
+this 
+is 
+a 
+practice
+set
+'''
+
+# question 3
+a = 35
+b = True
+c = "MySirG"
+d = 5.46
+e = 3+4j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# question 4
+a = 20
+b = 20
+print(id(a))
+print(id(b))
+
+# question 5
+import keyword
+print(keyword.kwlist)
+# question 6
+import keyword
+print(keyword.kwlist)
+
+
+# question -7
+help> keyword
+
+
+
+
+# question 8
+import a1
+print(a1.a)
+# this question is done with the help of vscode
+# question 9
+True, False, None
+# these keywords are written in capital letters in keywords
+
+# question 10
+from datetime import datetime
+dt = datetime.today();
+print(dt)
